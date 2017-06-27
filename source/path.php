5ff826fbdc8dbbf15a13b06fe0cfd03c15c4e203
Aron Wouters <lionar@mail.com>
2017-06-27 21:03:22 +0200

    Added path to path constructor.

--- source/path.php
+++ source/path.php
@@ -6,6 +6,6 @@ class path extends \ioc\provider
 {
 	public function register ( )
 	{
-		$this->container->instance ( 'path', new \path\collection );
+		$this->container->instance ( 'path', new \path\collection ( __DIR__ . '/../..' ) );
 	}
 }
